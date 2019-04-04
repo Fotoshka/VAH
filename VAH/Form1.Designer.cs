590bb88552e8747bcc058726e7e962f1d1368458
desfarik <desfarik@gmail.com>
2019-04-04 22:58:50 +0300

    init

---
+++ VAH/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace VAH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button startButton;
+            System.Windows.Forms.Button stopButton;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comPortComboBox = new System.Windows.Forms.ComboBox();
+            this.comPortSpeedComboBox = new System.Windows.Forms.ComboBox();
+            this.xScaleInput = new System.Windows.Forms.TextBox();
+            this.xScaleComboBox = new System.Windows.Forms.ComboBox();
+            this.yScaleComboBox = new System.Windows.Forms.ComboBox();
+            this.yScaleInput = new System.Windows.Forms.TextBox();
+            this.whiteWithBlackGrid = new System.Windows.Forms.RadioButton();
+            this.blackWithWhiteGrid = new System.Windows.Forms.RadioButton();
+            this.BackgroundColor = new System.Windows.Forms.GroupBox();
+            this.saveImageButton = new System.Windows.Forms.Button();
+            this.exportValuesButton = new System.Windows.Forms.Button();
+            this.lineColorComboBox = new System.Windows.Forms.ComboBox();
+            this.lineColorBox = new System.Windows.Forms.GroupBox();
+            this.comPortBox = new System.Windows.Forms.GroupBox();
+            this.comPortSpeedBox = new System.Windows.Forms.GroupBox();
+            this.xScaleBox = new System.Windows.Forms.GroupBox();
+            this.yScaleBox = new System.Windows.Forms.GroupBox();
+            this.resistorValues = new System.Windows.Forms.ListBox();
+            this.SaveValuesButton = new System.Windows.Forms.Button();
+            startButton = new System.Windows.Forms.Button();
+            stopButton = new System.Windows.Forms.Button();
+            this.BackgroundColor.SuspendLayout();
+            this.lineColorBox.SuspendLayout();
+            this.comPortBox.SuspendLayout();
+            this.comPortSpeedBox.SuspendLayout();
+            this.xScaleBox.SuspendLayout();
+            this.yScaleBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            startButton.Location = new System.Drawing.Point(598, 229);
+            startButton.Name = "startButton";
+            startButton.Size = new System.Drawing.Size(62, 31);
+            startButton.TabIndex = 15;
+            startButton.Text = "Start";
+            startButton.UseVisualStyleBackColor = true;
+            startButton.Click += new System.EventHandler(this.actionButton_Click);
+            // 
+            // stopButton
+            // 
+            stopButton.Location = new System.Drawing.Point(662, 229);
+            stopButton.Name = "stopButton";
+            stopButton.Size = new System.Drawing.Size(62, 31);
+            stopButton.TabIndex = 26;
+            stopButton.Text = "Stop";
+            stopButton.UseVisualStyleBackColor = true;
+            stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(2, 2);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(575, 546);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM5";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comPortComboBox
+            // 
+            this.comPortComboBox.FormattingEnabled = true;
+            this.comPortComboBox.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "COM11",
+            "COM12"});
+            this.comPortComboBox.Location = new System.Drawing.Point(14, 19);
+            this.comPortComboBox.Name = "comPortComboBox";
+            this.comPortComboBox.Size = new System.Drawing.Size(121, 21);
+            this.comPortComboBox.TabIndex = 2;
+            this.comPortComboBox.SelectedIndexChanged += new System.EventHandler(this.comPortComboBox_SelectedIndexChanged);
+            // 
+            // comPortSpeedComboBox
+            // 
+            this.comPortSpeedComboBox.FormattingEnabled = true;
+            this.comPortSpeedComboBox.Items.AddRange(new object[] {
+            "128000 bit/s",
+            "115200 bit/s",
+            "57600 bit/s",
+            "38400 bit/s",
+            "19200 bit/s",
+            "14400 bit/s",
+            "9600 bit/s",
+            "7200 bit/s",
+            "4800 bit/s",
+            "2400 bit/s",
+            "1800 bit/s",
+            "1200 bit/s",
+            "600 bit/s",
+            "300 bit/s"});
+            this.comPortSpeedComboBox.Location = new System.Drawing.Point(6, 19);
+            this.comPortSpeedComboBox.Name = "comPortSpeedComboBox";
+            this.comPortSpeedComboBox.Size = new System.Drawing.Size(121, 21);
+            this.comPortSpeedComboBox.TabIndex = 5;
+            this.comPortSpeedComboBox.SelectedIndexChanged += new System.EventHandler(this.comPortSpeedComboBox_SelectedIndexChanged);
+            // 
+            // xScaleInput
+            // 
+            this.xScaleInput.Location = new System.Drawing.Point(15, 18);
+            this.xScaleInput.Name = "xScaleInput";
+            this.xScaleInput.Size = new System.Drawing.Size(44, 20);
+            this.xScaleInput.TabIndex = 6;
+            this.xScaleInput.Text = "1";
+            this.xScaleInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // xScaleComboBox
+            // 
+            this.xScaleComboBox.FormattingEnabled = true;
+            this.xScaleComboBox.Items.AddRange(new object[] {
+            "V/div",
+            "mV/div",
+            "mkV/div"});
+            this.xScaleComboBox.Location = new System.Drawing.Point(64, 18);
+            this.xScaleComboBox.Name = "xScaleComboBox";
+            this.xScaleComboBox.Size = new System.Drawing.Size(121, 21);
+            this.xScaleComboBox.TabIndex = 8;
+            this.xScaleComboBox.SelectedIndexChanged += new System.EventHandler(this.xScaleComboBox_SelectedIndexChanged);
+            // 
+            // yScaleComboBox
+            // 
+            this.yScaleComboBox.FormattingEnabled = true;
+            this.yScaleComboBox.Items.AddRange(new object[] {
+            "A/div",
+            "mA/div",
+            "mkA/div"});
+            this.yScaleComboBox.Location = new System.Drawing.Point(63, 18);
+            this.yScaleComboBox.Name = "yScaleComboBox";
+            this.yScaleComboBox.Size = new System.Drawing.Size(121, 21);
+            this.yScaleComboBox.TabIndex = 11;
+            this.yScaleComboBox.SelectedIndexChanged += new System.EventHandler(this.yScaleComboBox_SelectedIndexChanged);
+            // 
+            // yScaleInput
+            // 
+            this.yScaleInput.Location = new System.Drawing.Point(14, 18);
+            this.yScaleInput.Name = "yScaleInput";
+            this.yScaleInput.Size = new System.Drawing.Size(44, 20);
+            this.yScaleInput.TabIndex = 9;
+            this.yScaleInput.Text = "100";
+            this.yScaleInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // whiteWithBlackGrid
+            // 
+            this.whiteWithBlackGrid.AutoSize = true;
+            this.whiteWithBlackGrid.Location = new System.Drawing.Point(14, 42);
+            this.whiteWithBlackGrid.Name = "whiteWithBlackGrid";
+            this.whiteWithBlackGrid.Size = new System.Drawing.Size(124, 17);
+            this.whiteWithBlackGrid.TabIndex = 12;
+            this.whiteWithBlackGrid.Text = "White with black grid";
+            this.whiteWithBlackGrid.UseVisualStyleBackColor = true;
+            this.whiteWithBlackGrid.CheckedChanged += new System.EventHandler(this.buttonGroup_CheckedChanged);
+            // 
+            // blackWithWhiteGrid
+            // 
+            this.blackWithWhiteGrid.AutoSize = true;
+            this.blackWithWhiteGrid.Checked = true;
+            this.blackWithWhiteGrid.Location = new System.Drawing.Point(14, 19);
+            this.blackWithWhiteGrid.Name = "blackWithWhiteGrid";
+            this.blackWithWhiteGrid.Size = new System.Drawing.Size(122, 17);
+            this.blackWithWhiteGrid.TabIndex = 13;
+            this.blackWithWhiteGrid.TabStop = true;
+            this.blackWithWhiteGrid.Text = "Black with white grid";
+            this.blackWithWhiteGrid.UseVisualStyleBackColor = false;
+            this.blackWithWhiteGrid.CheckedChanged += new System.EventHandler(this.buttonGroup_CheckedChanged);
+            // 
+            // BackgroundColor
+            // 
+            this.BackgroundColor.Controls.Add(this.blackWithWhiteGrid);
+            this.BackgroundColor.Controls.Add(this.whiteWithBlackGrid);
+            this.BackgroundColor.Location = new System.Drawing.Point(584, 160);
+            this.BackgroundColor.Name = "BackgroundColor";
+            this.BackgroundColor.Size = new System.Drawing.Size(147, 63);
+            this.BackgroundColor.TabIndex = 14;
+            this.BackgroundColor.TabStop = false;
+            this.BackgroundColor.Text = "Background Color";
+            // 
+            // saveImageButton
+            // 
+            this.saveImageButton.Location = new System.Drawing.Point(736, 229);
+            this.saveImageButton.Name = "saveImageButton";
+            this.saveImageButton.Size = new System.Drawing.Size(66, 31);
+            this.saveImageButton.TabIndex = 16;
+            this.saveImageButton.Text = ".jpg";
+            this.saveImageButton.UseVisualStyleBackColor = true;
+            // 
+            // exportValuesButton
+            // 
+            this.exportValuesButton.Location = new System.Drawing.Point(597, 517);
+            this.exportValuesButton.Name = "exportValuesButton";
+            this.exportValuesButton.Size = new System.Drawing.Size(121, 31);
+            this.exportValuesButton.TabIndex = 17;
+            this.exportValuesButton.Text = "Export Rd values";
+            this.exportValuesButton.UseVisualStyleBackColor = true;
+            // 
+            // lineColorComboBox
+            // 
+            this.lineColorComboBox.FormattingEnabled = true;
+            this.lineColorComboBox.Items.AddRange(new object[] {
+            "Red",
+            "Black",
+            "White",
+            "Blue",
+            "Purple",
+            "Yellow"});
+            this.lineColorComboBox.Location = new System.Drawing.Point(6, 19);
+            this.lineColorComboBox.Name = "lineColorComboBox";
+            this.lineColorComboBox.Size = new System.Drawing.Size(121, 21);
+            this.lineColorComboBox.TabIndex = 18;
+            this.lineColorComboBox.SelectedIndexChanged += new System.EventHandler(this.lineColorComboBox_SelectedIndexChanged);
+            // 
+            // lineColorBox
+            // 
+            this.lineColorBox.Controls.Add(this.lineColorComboBox);
+            this.lineColorBox.Location = new System.Drawing.Point(743, 160);
+            this.lineColorBox.Name = "lineColorBox";
+            this.lineColorBox.Size = new System.Drawing.Size(140, 63);
+            this.lineColorBox.TabIndex = 19;
+            this.lineColorBox.TabStop = false;
+            this.lineColorBox.Text = "Line color";
+            // 
+            // comPortBox
+            // 
+            this.comPortBox.Controls.Add(this.comPortComboBox);
+            this.comPortBox.Location = new System.Drawing.Point(583, 2);
+            this.comPortBox.Name = "comPortBox";
+            this.comPortBox.Size = new System.Drawing.Size(148, 46);
+            this.comPortBox.TabIndex = 20;
+            this.comPortBox.TabStop = false;
+            this.comPortBox.Text = "COM-Port";
+            // 
+            // comPortSpeedBox
+            // 
+            this.comPortSpeedBox.Controls.Add(this.comPortSpeedComboBox);
+            this.comPortSpeedBox.Location = new System.Drawing.Point(743, 2);
+            this.comPortSpeedBox.Name = "comPortSpeedBox";
+            this.comPortSpeedBox.Size = new System.Drawing.Size(140, 46);
+            this.comPortSpeedBox.TabIndex = 21;
+            this.comPortSpeedBox.TabStop = false;
+            this.comPortSpeedBox.Text = "COM-Port speed";
+            // 
+            // xScaleBox
+            // 
+            this.xScaleBox.Controls.Add(this.xScaleInput);
+            this.xScaleBox.Controls.Add(this.xScaleComboBox);
+            this.xScaleBox.Location = new System.Drawing.Point(647, 54);
+            this.xScaleBox.Name = "xScaleBox";
+            this.xScaleBox.Size = new System.Drawing.Size(200, 48);
+            this.xScaleBox.TabIndex = 22;
+            this.xScaleBox.TabStop = false;
+            this.xScaleBox.Text = "X-Scale ";
+            // 
+            // yScaleBox
+            // 
+            this.yScaleBox.Controls.Add(this.yScaleInput);
+            this.yScaleBox.Controls.Add(this.yScaleComboBox);
+            this.yScaleBox.Location = new System.Drawing.Point(647, 106);
+            this.yScaleBox.Name = "yScaleBox";
+            this.yScaleBox.Size = new System.Drawing.Size(200, 48);
+            this.yScaleBox.TabIndex = 23;
+            this.yScaleBox.TabStop = false;
+            this.yScaleBox.Text = "Y-Scale";
+            // 
+            // resistorValues
+            // 
+            this.resistorValues.FormattingEnabled = true;
+            this.resistorValues.Location = new System.Drawing.Point(597, 266);
+            this.resistorValues.Name = "resistorValues";
+            this.resistorValues.Size = new System.Drawing.Size(286, 238);
+            this.resistorValues.TabIndex = 24;
+            // 
+            // SaveValuesButton
+            // 
+            this.SaveValuesButton.Location = new System.Drawing.Point(817, 229);
+            this.SaveValuesButton.Name = "SaveValuesButton";
+            this.SaveValuesButton.Size = new System.Drawing.Size(66, 31);
+            this.SaveValuesButton.TabIndex = 25;
+            this.SaveValuesButton.Text = ".txt";
+            this.SaveValuesButton.UseVisualStyleBackColor = true;
+            this.SaveValuesButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(897, 560);
+            this.Controls.Add(stopButton);
+            this.Controls.Add(this.SaveValuesButton);
+            this.Controls.Add(this.resistorValues);
+            this.Controls.Add(this.yScaleBox);
+            this.Controls.Add(this.xScaleBox);
+            this.Controls.Add(this.comPortSpeedBox);
+            this.Controls.Add(this.comPortBox);
+            this.Controls.Add(this.lineColorBox);
+            this.Controls.Add(this.exportValuesButton);
+            this.Controls.Add(this.saveImageButton);
+            this.Controls.Add(startButton);
+            this.Controls.Add(this.BackgroundColor);
+            this.Controls.Add(this.AnT);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "C-V Сhr";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.BackgroundColor.ResumeLayout(false);
+            this.BackgroundColor.PerformLayout();
+            this.lineColorBox.ResumeLayout(false);
+            this.comPortBox.ResumeLayout(false);
+            this.comPortSpeedBox.ResumeLayout(false);
+            this.xScaleBox.ResumeLayout(false);
+            this.xScaleBox.PerformLayout();
+            this.yScaleBox.ResumeLayout(false);
+            this.yScaleBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comPortComboBox;
+        private System.Windows.Forms.ComboBox comPortSpeedComboBox;
+        private System.Windows.Forms.TextBox xScaleInput;
+        private System.Windows.Forms.ComboBox xScaleComboBox;
+        private System.Windows.Forms.ComboBox yScaleComboBox;
+        private System.Windows.Forms.TextBox yScaleInput;
+        private System.Windows.Forms.RadioButton whiteWithBlackGrid;
+        private System.Windows.Forms.RadioButton blackWithWhiteGrid;
+        private System.Windows.Forms.GroupBox BackgroundColor;
+        private System.Windows.Forms.Button saveImageButton;
+        private System.Windows.Forms.Button exportValuesButton;
+        private System.Windows.Forms.ComboBox lineColorComboBox;
+        private System.Windows.Forms.GroupBox lineColorBox;
+        private System.Windows.Forms.GroupBox comPortBox;
+        private System.Windows.Forms.GroupBox comPortSpeedBox;
+        private System.Windows.Forms.GroupBox xScaleBox;
+        private System.Windows.Forms.GroupBox yScaleBox;
+        private System.Windows.Forms.ListBox resistorValues;
+        private System.Windows.Forms.Button SaveValuesButton;
+    }
+}
+
---
+++ VAH/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.FreeGlut;
+
+namespace VAH
+{
+    public partial class Form1 : Form
+    {
+        private List<float[]> points = new List<float[]>();
+        private string comPortName = "COM1";
+        private string comPortSpeed = "128000";
+        private string xScaleValue = "кВ/дел.";
+        private string yScaleValue = "A/дел.";
+        private string backgroundColor = "Black";
+        private string lineColor = "Red";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            AnT.InitializeContexts();
+            // инициализация Glut 
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+
+            Gl.glClearColor(0, 0, 0, 1); // отчитка окна 
+            //Gl.glViewport(0, 0, AnT.Width, AnT.Height); // установка порта вывода в соответствии с размерами элемента anT 
+            //Gl.glViewport(-10f, 10f, 20f, 20f);
+
+            // настройка проекции 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+
+            //Glu.gluOrtho2D(0.0, (float)AnT.Width, (float)AnT.Height, 0);
+            Glu.gluOrtho2D(-5f, 5f, -0.0005f, 0.0005f);
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+
+
+       
+        }
+
+        void log(string s)
+        {
+            resistorValues.Items.Add(s);
+            resistorValues.SelectedIndex = resistorValues.Items.Count - 1;
+            resistorValues.SelectedIndex = -1;
+        //    while (resistorValues.Items.Count > 20)
+        //      resistorValues.Items.RemoveAt(0);
+        }
+
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+            Gl.glVertex2d(x0, y0);
+            Gl.glVertex2d(x1, y1);
+            Gl.glEnd();
+        }
+
+        private void incomingData(string data)
+        {
+            //log(data); //вывод 4-х приходящих значений в лог
+            List<string> ar = data.Split(':').ToList();
+            if (ar.Count != 4)
+                return;
+
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT); // очищаем буфер цвета 
+            Gl.glLoadIdentity(); // очищаем текущую матрицу 
+
+            Gl.glColor3f(1f, 1f, 1f);
+            //drawLine(-1, 0, 1, 0);
+            for (int i = -100; i < 100; i++)
+            {
+                float color = ((i % 10) == 0) ? 0.5f : 0.1f;
+                if (0 == i)
+                    color = 1f;
+                Gl.glColor3f(color, color, color);
+                drawLine((float)i / 10, -1, (float)i / 10, 1);
+                drawLine(-10, (float)i / 100000, 10, (float)i / 100000);
+            }
+
+            //LOG(OCR2A);LOG(":");LOG(analogRead(0));LOG(":");LOG(analogRead(1));LOG(":");LOGLN(analogRead(2));
+
+            float aref = 5, r1 = 10000;            
+            float a0 = (float)Int16.Parse(ar[1]) * aref / 1024;
+            float a1 = (float)Int16.Parse(ar[2]) * aref / 1024;
+            float a2 = (float)Int16.Parse(ar[3]) * aref / 1024;
+            float uD = a1 - a0;
+            float iD = (a0 - a2) / r1;
+            log("U = " + uD + "      " + " I = " + iD);
+
+            float[] point = {uD, iD};
+            points.Add(point);
+
+            float[] prevArr = null;
+            foreach (float[] curArr in points)
+            {
+                if (null != prevArr)
+                {
+                    Gl.glColor3f(1f, 0f, 0f);
+                    Gl.glBegin(Gl.GL_LINE_STRIP);
+                    Gl.glVertex2d(prevArr[0], prevArr[1]);
+                    Gl.glVertex2d(curArr[0], curArr[1]);
+                    Gl.glEnd();
+                }                
+                prevArr = curArr;
+            }
+            
+            Gl.glFlush();
+            AnT.Invalidate();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            Action<string> idata = incomingData;
+            while(serialPort1.BytesToRead > 0)
+                BeginInvoke(idata, serialPort1.ReadLine());
+        }
+
+        private void comPortComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox box = (ComboBox)sender;
+            this.comPortName = box.SelectedItem.ToString();
+
+        }
+
+        private void comPortSpeedComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox box = (ComboBox)sender;
+            this.comPortSpeed = box.SelectedItem.ToString();
+        }
+
+        private void xScaleComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox box = (ComboBox)sender;
+            this.xScaleValue = box.SelectedItem.ToString();
+        }
+
+        private void yScaleComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox box = (ComboBox)sender;
+            this.yScaleValue = box.SelectedItem.ToString();
+        }
+
+        private void lineColorComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox box = (ComboBox)sender;
+            this.backgroundColor = box.SelectedItem.ToString();
+        }
+
+        private void buttonGroup_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void actionButton_Click(object sender, EventArgs e)
+     
+        {
+            serialPort1.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comPortComboBox.SelectedIndex = 5;
+            comPortSpeedComboBox.SelectedIndex = 1;
+            xScaleComboBox.SelectedIndex = 0;
+            yScaleComboBox.SelectedIndex = 1;
+            lineColorComboBox.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+     
+    }
+}
